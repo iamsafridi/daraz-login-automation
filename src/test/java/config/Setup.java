bd587b35d97c87d30c3650e42a10e567bf53e090
shahid Afridi <shahidafridi.baymax@gmail.com>
2024-03-12 20:41:22 +0600

    done

--- src/test/java/config/Setup.java
+++ src/test/java/config/Setup.java
@@ -3,6 +3,7 @@ package config;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
@@ -14,11 +15,14 @@ public class Setup {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+    @BeforeMethod
+    public void navigateToLoginPage() {
         driver.get("https://member.daraz.com.bd/user/login");
     }
 
     @AfterTest
     public void quitDriver(){
-//        driver.quit();
+        driver.close();
     }
 }
--- src/test/java/testrunner/LoginTestRunner.java
+++ src/test/java/testrunner/LoginTestRunner.java
@@ -2,14 +2,39 @@ package testrunner;
 
 import config.Setup;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import page.LoginPage;
 
 public class LoginTestRunner extends Setup {
-        @Test(description = "User can login successfully",priority = 1)
-        public void doLogin(){
-            LoginPage loginPage = new LoginPage(driver);
-            loginPage.doLogin("shahidafridi.baymax@gmail.com","Afridi@@afridI144");
-        }
-        init
+    @Test(priority = 2,description = "User cannot login with empty field")
+    public void emptyField(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.doLogin("","");
+        //*[@id="container"]/div/div[2]/form/div/div[1]/div[1]/span
+        String message = driver.findElements(By.xpath("//*[@id=\"container\"]/div/div[2]/form/div/div[1]/div[1]/span")).get(0).getText();
+        Assert.assertTrue(message.contains("You can't leave this empty."));
+    }
+    @Test(priority = 3,description = "User cannot login with invalid email")
+    public void invalidEmail(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.doLogin("asd","");
+    }
+    @Test(priority = 4,description = "User cannot login with valid email invalid password")
+    public void invalidPassword(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.doLogin("asd@gmail.com","123");
+    }
+    @Test(priority = 1,description = "User can login successfully")
+    public void doLogin(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.doLogin("shahidafridi.baymax@gmail.com","Afridi@@afridI144");
+        String message = driver.findElements(By.className("dashboard-mod-title")).get(0).getText();
+        Assert.assertTrue(message.contains("Personal Profile"));
+        driver.get("https://member.daraz.com.bd/user/logout");
+
+    }
+
 }
